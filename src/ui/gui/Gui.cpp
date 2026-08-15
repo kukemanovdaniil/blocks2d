d98ae14223650d8b6230d95e25179a2c77f5fc16
KukemanovDaniil <danilbelov900@gmail.com>
2026-08-15 20:51:51 +0800

    delete tgui, add rmlui

--- src/ui/gui/Gui.cpp
+++
@@ -1,9 +0,0 @@
-#include "src/ui/gui/Gui.hpp"
-
-void Ui::Gui::draw() noexcept {
-    m_gui.draw();
-}
-
-void Ui::Gui::handleEvent(const sf::Event& event) const noexcept {
-    m_gui.handleEvent(event);
-}
