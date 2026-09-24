4d854f1e89cb021275e3e3bb69173fde14573f23
KukemanovDaniil <danilbelov900@gmail.com>
2026-09-25 03:07:27 +0800

    new textures & version 0.1.1

--- src/core/CMakeConfig.hpp
+++ src/core/CMakeConfig.hpp
@@ -7,7 +7,7 @@ namespace Config {
     // CONSTANS
     
     inline constexpr const char* TITLE{"blocks2d"};
-    inline constexpr const char* VERSION{"0.1.0"};
+    inline constexpr const char* VERSION{"0.1.1"};
 
-    inline constexpr const char* FULL_TITLE{"blocks2d 0.1.0"};
+    inline constexpr const char* FULL_TITLE{"blocks2d 0.1.1"};
 }
